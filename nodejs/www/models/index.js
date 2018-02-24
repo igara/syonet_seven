d6f2a4f21c4844b9887dd7d0c32953206fd68639
Igarashi Syo <igara1119@gmail.com>
2018-02-24 21:18:17 +0900

    環境変数を用いてアクセスできるように対応

--- nodejs/www/models/index.js
+++ nodejs/www/models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://syonet:syonet@mongodb/admin?authMechanism=SCRAM-SHA-1')
+mongoose.connect(`mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@mongodb/admin?authMechanism=SCRAM-SHA-1`)
 
 module.exports = mongoose
