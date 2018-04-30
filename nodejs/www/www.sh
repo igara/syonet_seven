d42036b0e718e39e603ecc9752a195992cc5f791
Igarashi Syo <igara1119@gmail.com>
2018-04-30 11:43:49 +0900

    flow導入

--- nodejs/www/www.sh
+++ nodejs/www/www.sh
@@ -4,9 +4,9 @@ npm install -g yarn
 yarn
 if [ $WWW_ENV = "local" ]; then
 yarn build
-yarn local_sev_start
+yarn local_dev_start
 # yarn local_front_start
 elif [ $WWW_ENV = "production" ]; then
 yarn build
-yarn start
+yarn prod_start
 fi
