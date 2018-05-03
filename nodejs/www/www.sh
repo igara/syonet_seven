4d0a632280560866c64a43c42cf09c34571ad65c
Igarashi Syo <igara1119@gmail.com>
2018-05-03 22:01:42 +0900

    タイポ部分を修正

--- nodejs/www/www.sh
+++ nodejs/www/www.sh
@@ -4,7 +4,7 @@ npm install -g yarn
 yarn
 if [ $WWW_ENV = "local" ]; then
 yarn build
-yarn local_sev_start
+yarn local_dev_start
 # yarn local_front_start
 elif [ $WWW_ENV = "production" ]; then
 yarn build
