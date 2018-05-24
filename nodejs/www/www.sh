eda329026d233672fe166c47e8c71d5c2b0ee18e
Igarashi Syo <igara1119@gmail.com>
2018-05-25 06:56:28 +0900

    syonetように修正

--- nodejs/www/www.sh
+++ nodejs/www/www.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 if [ $WWW_ENV = "local" ]; then
+yarn
 yarn build
 yarn start
 # yarn local_front_start
 elif [ $WWW_ENV = "production" ]; then
+yarn
 yarn build
-yarn server_prod
+yarn start
 fi
