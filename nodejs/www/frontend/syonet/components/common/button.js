8021735f83e4d0a20cb39cbfb0311844ef2e198c
Igarashi Syo <igara1119@gmail.com>
2018-06-13 07:51:10 +0900

    ファイル名を小文字に修正

---
+++ nodejs/www/frontend/syonet/components/common/button.js
@@ -0,0 +1,44 @@
+import {m} from '../../mithril'
+import {ButtonStyle} from '../../styles'
+
+/**
+ * ボタンを表示するコンポーネント
+ */
+export default class ButtonComponent {
+
+	Stores
+
+	/**
+	 * propsで渡されてきた押下時のイベントハンドラ
+	 */
+	OnClickHandler
+
+	Href
+
+	Children
+
+	/**
+	 * @constructor
+	 * @param {Vnode<A, this>} vnode 
+	 */
+	constructor(vnode) {
+		this.Stores = vnode.attrs.Stores
+		this.OnClickHandler = vnode.attrs.OnClickHandler
+		this.Href = vnode.attrs.Href
+		this.Children = vnode.children.find(c => c.children)
+	}
+
+	/**
+	 * Lifecycle: Creates a view out of virtual elements.
+	 */
+	view() {
+		return (
+			<button class={ButtonStyle.button} onclick={() => this.OnClickHandler()}>
+				{this.Href ?
+					<a href={this.Href}>{this.Children}</a> :
+					<a>{this.Children}</a>
+				}
+			</button>
+		)
+	}
+}
