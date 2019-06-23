a10dc8b8f24cf71dc1291d12b6d22f9563624749
Syo Igarashi <igara1119@gmail.com>
2019-06-23 21:24:46 +0900

    modify: fix production(when remove https-portal code

--- nginx/cert.sh
+++ nginx/cert.sh
@@ -17,7 +17,7 @@ if [ "$WWW_ENV" = "production" ]; then
         chmod 400 "$key_file"
     fi
 
-    cp /nginx/syonet.work/cert.conf /etc/nginx/conf.d/nginx.conf
+    cp /nginx/syonet.work/cert.conf /etc/nginx/conf.d/default.conf
     nginx
 
     if [ ! -e "/etc/letsencrypt/initialize" ]; then
@@ -31,7 +31,7 @@ if [ "$WWW_ENV" = "production" ]; then
     certbot renew
 
     nginx -s stop
-    cp /nginx/syonet.work/www.conf /etc/nginx/conf.d/nginx.conf
+    cp /nginx/syonet.work/www.conf /etc/nginx/conf.d/default.conf
 fi
 
 echo "success: updated certification"
