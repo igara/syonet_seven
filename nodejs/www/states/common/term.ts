8348ce7979e214fe02ff641a25aac11cef6a6d96
garasyo <igara1119@gmail.com>
2020-04-07 07:11:49 +0900

    modify: css animation.

--- nodejs/www/states/common/term.ts
+++ nodejs/www/states/common/term.ts
@@ -23,5 +23,5 @@ export const termReducer = reducerWithInitialState(initialState)
    * 閉じるを押下したときの処理
    */
   .case(termActions.onClickClose, state => {
-    return { ...state, dispFlag: false, chengedDispFlag: true };
+    return { ...state, dispFlag: false, chengedDispFlag: false };
   });
