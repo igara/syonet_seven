bdd31983e18830e684b51829e68a4f7e2f86a2ec
garasyo <igara1119@gmail.com>
2020-07-20 12:55:55 +0900

    modify: memory.

--- nodejs/www/www.sh
+++ nodejs/www/www.sh
@@ -4,6 +4,8 @@ if [ $NPM_INSTALL = "1" ]; then
 npm install -g npm
 npm run clean:all
 npm ci
+npm run sw:build
+npm run build
 fi
 if [ $WWW_DOMAIN = "localhost" ]; then
 npm run dev
@@ -11,8 +13,5 @@ elif [ $WWW_DOMAIN = "local.syonet.work" ]; then
 npm run dev
 elif [ $WWW_DOMAIN = "syonet.work" ]; then
 export NODE_ENV=production
-npm run sw:build
-npm run build
 npm run start
-# npm run production
 fi
